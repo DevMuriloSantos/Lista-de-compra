dacc5fb68788512628fb09f845b84a06657ea9ff
Murilo <murilopaulo051@gmail.com>
2025-07-11 23:21:57 -0300

    atualização 1.1

--- main.js
+++ main.js
@@ -1,38 +1,41 @@
 const add = document.getElementById("add");
-    const ul = document.getElementById("ul");
+const ul = document.getElementById("ul");
+const lixeira = document.getElementById("lixeira")
 
-    for (let i = 0; i < localStorage.length; i++) {
-        const key = localStorage.key(i);
+for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    const li = document.createElement("li");
+
+    li.textContent = key;
+    li.innerHTML += '<input type="checkbox" name="check" class="marcado">';
+    ul.appendChild(li);
+}
+
+add.addEventListener("click", function (event) {
+    event.preventDefault(); // Impede o envio do formulário
+
+    const item = document.getElementById("list").value;
+
+    if (item) { // Verifica se o input não está vazio
         const li = document.createElement("li");
-        li.textContent = key;
+        li.textContent = item; // Define o texto do <li>
 
-        ul.appendChild(li);
+        for (let i = 0; i < ul.children.length; i++) {
+            if (item == ul.children[i].textContent) {
+                alert("Item já adicionado! Por favor digite outro item.");
+
+                document.getElementById("list").value = "";
+                return; // Sai da função para não adicionar o item novamente
+            }
+        };
+
+
+        localStorage.setItem(item, item); // Adiciona o item ao localStorage (chave, valor)
+
+        ul.appendChild(li); // Adiciona o <li> à lista
+        alert("Item adicionado com sucesso!"); // Alerta de sucesso
+        li.innerHTML += '<input type="checkbox" name="t" id="t" onchange="ale()">';
+
+        document.getElementById("list").value = "";
     }
-
-    add.addEventListener("click", function (event) {
-        event.preventDefault(); // Impede o envio do formulário
-
-        const item = document.getElementById("list").value;
-
-        if (item) { // Verifica se o input não está vazio
-            const li = document.createElement("li");
-            li.textContent = item; // Define o texto do <li>
-
-            for (let i = 0; i < ul.children.length; i++) {
-                if (item == ul.children[i].textContent) {
-                    alert("Item já adicionado! Por favor digite outro item.");
-
-                    document.getElementById("list").value = "";
-                    return; // Sai da função para não adicionar o item novamente
-                }
-            };
-
-            
-            localStorage.setItem(item, item); // Adiciona o item ao localStorage (chave, valor)
-            
-            ul.appendChild(li); // Adiciona o <li> à lista
-            alert("Item adicionado com sucesso!"); // Alerta de sucesso
-
-            document.getElementById("list").value = "";
-        }
-    });
+});
