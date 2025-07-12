b0d71b1f827eb6b59f5de60ab7b0cb0c07195865
Murilo <murilopaulo051@gmail.com>
2025-07-12 03:22:52 -0300

    atualização 1.2

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ add.addEventListener("click", function (event) {
     const item = document.getElementById("list").value;
 
     if (item) { // Verifica se o input não está vazio
-        const li = document.createElement("li");
+        let li = document.createElement("li");
         li.textContent = item; // Define o texto do <li>
 
         for (let i = 0; i < ul.children.length; i++) {
@@ -32,10 +32,46 @@ add.addEventListener("click", function (event) {
 
         localStorage.setItem(item, item); // Adiciona o item ao localStorage (chave, valor)
 
+        li.innerHTML += '<input type="checkbox" name="marcado" class="marcado">';
+        li.classList.add('li')
         ul.appendChild(li); // Adiciona o <li> à lista
         alert("Item adicionado com sucesso!"); // Alerta de sucesso
-        li.innerHTML += '<input type="checkbox" name="t" id="t" onchange="ale()">';
 
         document.getElementById("list").value = "";
     }
 });
+
+let marcado = document.querySelectorAll('.marcado');
+let li = document.querySelector("li");
+
+ul.addEventListener('change', function (e) {
+    // Se o checkbox foi marcado, aplica o line-through no <li> correspondente
+    if (e.target.checked) {
+        e.target.parentElement.style.textDecoration = 'line-through';
+
+        lixeira.style.color = "#a72525"
+    }
+
+    else {
+        e.target.parentElement.style.textDecoration = '';
+        lixeira.style.color = "white"
+    }
+});
+
+lixeira.addEventListener('click', function () {
+    const pergunta = prompt("Deseja realmente apagar os itens selecionados ?")
+
+    if (pergunta == "Sim") {
+        // Seleciona todos os checkboxes marcados
+        const marcados = document.querySelectorAll('.marcado:checked');
+        marcados.forEach(checkbox => {
+            const li = checkbox.parentElement;
+            const item = li.childNodes[0].textContent.trim();
+            localStorage.removeItem(item); // Remove do localStorage
+            li.remove(); // Remove o <li> da lista
+        });
+
+        alert("Item(s) removido com sucesso!")
+    }
+});
+
